d97700534fda5fe22827472cffea09a405e42ddd
HR/大橋政也 <chopsticks@hoshinoresorts.com>
2024-09-21 23:16:40 +0900

    deploy setting github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://akabana0524.github.io/task-memo/'
   plugins: [vue()],
 })
