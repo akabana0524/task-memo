f39315a7cf2b341b32073c8b4de42ecf9eb786a8
HR/大橋政也 <chopsticks@hoshinoresorts.com>
2024-09-27 23:51:18 +0900

    history implement

--- src/components/TaskListItem.vue
+++ src/components/TaskListItem.vue
@@ -2,17 +2,24 @@
   <v-list-item>
     <template v-slot:prepend>
       <v-list-item-action start>
-        <v-checkbox-btn @change="removeTask" />
+        <v-checkbox-btn @change="completeTask" />
       </v-list-item-action>
     </template>
     <template v-slot:title>
-      <v-text-field
+      <v-combobox
         v-model="task.title"
+        :items="taskHistoryTitles"
         variant="plain"
         hide-details
         single-line
+        hide-spin-buttons
         density="compact"
       />
+    </template>
+    <template v-slot:subtitle>
+      <div v-if="completedAt != null">
+        前回: {{ completeDifferenceInDays }}日前 ({{ completedAt }})
+      </div>
     </template>
   </v-list-item>
 </template>
@@ -24,8 +31,22 @@ interface TaskListItemProps {
   task: Task;
 }
 const { task } = defineProps<TaskListItemProps>();
-const { removeTask: _removeTask } = useTask();
-function removeTask() {
-  nextTick(() => _removeTask(task.taskId));
+const {
+  completeTask: _completeTask,
+  taskHistoryTitles,
+  findTaskHistory,
+} = useTask();
+const taskHistory = findTaskHistory(task.title);
+const completedAt = taskHistory
+  ? new Date(taskHistory.completedAt).toLocaleString()
+  : null;
+const completeDifferenceInDays = taskHistory
+  ? Math.floor(
+      (new Date().getTime() - new Date(taskHistory.completedAt).getTime()) /
+        (1000 * 60 * 60 * 24)
+    )
+  : 0;
+function completeTask() {
+  nextTick(() => _completeTask(task.taskId));
 }
 </script>
--- src/composables/Task.ts
+++ src/composables/Task.ts
@@ -1,20 +1,38 @@
-import { ref, watch } from "vue";
+import { computed, ref, watch } from "vue";
 
 export type TaskId = string;
 export type TaskTitle = string;
+export type CreatedAt = string;
+export type CompletedAt = string;
 export interface Task {
   taskId: TaskId;
   title: TaskTitle;
-  createdAt: Date;
+  createdAt: CreatedAt;
+}
+
+export interface TaskHistory {
+  title: TaskTitle;
+  completedAt: CompletedAt;
 }
 
 // global
 const tasks = ref<Task[]>([]);
+const taskHistories = ref<TaskHistory[]>([]);
+
+// save
 watch(
   tasks,
   (v) => {
-    console.log("save localStorage");
+    console.log("save tasks");
     localStorage.setItem("tasks", JSON.stringify(v));
+  },
+  { deep: true }
+);
+watch(
+  taskHistories,
+  (v) => {
+    console.log("save taskHistories");
+    localStorage.setItem("taskHistories", JSON.stringify(v));
   },
   { deep: true }
 );
@@ -24,18 +42,48 @@ export function useTask() {
   function loadTasks() {
     var tasksJson = localStorage.getItem("tasks");
     if (tasksJson) {
-      console.log({ tasksJson });
       var _tasks: Task[] = JSON.parse(tasksJson);
       tasks.value.splice(0, tasks.value.length, ..._tasks);
+    }
+    var taskHistoriesJson = localStorage.getItem("taskHistories");
+    if (taskHistoriesJson) {
+      var _taskHistories: TaskHistory[] = JSON.parse(taskHistoriesJson);
+      taskHistories.value.splice(
+        0,
+        taskHistories.value.length,
+        ..._taskHistories
+      );
     }
   }
 
   function addBlankTask() {
     tasks.value.push({
       taskId: crypto.randomUUID(),
-      createdAt: new Date(),
+      createdAt: new Date().toISOString(),
       title: "",
     });
+  }
+  function getTask(taskId: TaskId) {
+    return tasks.value.find((v) => v.taskId == taskId)!;
+  }
+  function findTaskHistory(taskTitle: TaskTitle) {
+    return taskHistories.value.find((v) => v.title == taskTitle) || null;
+  }
+
+  function completeTask(taskId: TaskId) {
+    var task = getTask(taskId);
+    if (task.title) {
+      var taskHistory = findTaskHistory(task.title);
+      if (taskHistory) {
+        taskHistory.completedAt = new Date().toISOString();
+      } else {
+        taskHistories.value.push({
+          title: task.title,
+          completedAt: new Date().toISOString(),
+        });
+      }
+    }
+    removeTask(taskId);
   }
 
   function removeTask(taskId: TaskId) {
@@ -45,5 +93,16 @@ export function useTask() {
     }
   }
 
-  return { tasks, addBlankTask, removeTask, loadTasks };
+  const taskHistoryTitles = computed(() =>
+    taskHistories.value.map((v) => v.title)
+  );
+  return {
+    tasks,
+    addBlankTask,
+    completeTask,
+    removeTask,
+    loadTasks,
+    taskHistoryTitles,
+    findTaskHistory,
+  };
 }
