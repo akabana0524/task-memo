a04d7fb06fa69dcf5de12f5d946a44e5772016e4
HR/大橋政也 <chopsticks@hoshinoresorts.com>
2024-09-27 23:06:48 +0900

    removeTask use nextTick

--- src/components/TaskListItem.vue
+++ src/components/TaskListItem.vue
@@ -2,7 +2,7 @@
   <v-list-item>
     <template v-slot:prepend>
       <v-list-item-action start>
-        <v-checkbox-btn @change="() => removeTask(task.taskId)" />
+        <v-checkbox-btn @change="removeTask" />
       </v-list-item-action>
     </template>
     <template v-slot:title>
@@ -17,11 +17,15 @@
   </v-list-item>
 </template>
 <script lang="ts" setup>
+import { nextTick } from "vue";
 import { Task, useTask } from "../composables/Task";
 
 interface TaskListItemProps {
   task: Task;
 }
 const { task } = defineProps<TaskListItemProps>();
-const { removeTask } = useTask();
+const { removeTask: _removeTask } = useTask();
+function removeTask() {
+  nextTick(() => _removeTask(task.taskId));
+}
 </script>
