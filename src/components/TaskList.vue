05b9b47f368bc928d235afffb55e8cc0cc9c3d1f
HR/大橋政也 <chopsticks@hoshinoresorts.com>
2024-09-28 18:43:19 +0900

    draggable abolition

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -1,33 +1,21 @@
 <template>
-  <draggable
-    v-model="tasks"
-    tag="v-list"
-    group="task"
-    @start="drag = true"
-    @end="drag = false"
-    item-key="taskId"
-    :component-data="{ transition: 'slide-y-transition' }"
-  >
-    <template #item="{ element: task }">
-      <TaskListItem :task="task" :key="task.taskId">{{
-        task.title
-      }}</TaskListItem>
-    </template>
-    <template #footer>
-      <v-list-item @click="addBlankTask">
-        <v-row>
-          <v-spacer />
-          <v-col cols="auto"><v-icon>mdi-plus</v-icon></v-col>
-          <v-spacer />
-        </v-row>
-      </v-list-item>
-    </template>
-  </draggable>
+  <v-list>
+    <TaskListItem v-for="task in tasks" :key="task.taskId" :task="task">{{
+      task.title
+    }}</TaskListItem>
+
+    <v-list-item @click="addBlankTask">
+      <v-row>
+        <v-spacer />
+        <v-col cols="auto"><v-icon>mdi-plus</v-icon></v-col>
+        <v-spacer />
+      </v-row>
+    </v-list-item>
+  </v-list>
 </template>
 
 <script lang="ts" setup>
 import { ref } from "vue";
-import draggable from "vuedraggable";
 import { useTask } from "../composables/Task";
 import TaskListItem from "./TaskListItem.vue";
 
